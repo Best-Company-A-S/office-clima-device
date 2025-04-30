934de04c5f89f6673d5adcd2969e464bca323dfd
Anya Roza Kragh <anya0293@zbc.dk>
2025-04-30 12:09:56 +0200

    Started working on V0.8.0

---
+++ H2Climate_v0.8.0_uno_r4/src/network/NetworkManager.h
@@ -0,0 +1,29 @@
+#ifndef NETWORK_MANAGER_H
+#define NETWORK_MANAGER_H
+
+#include "../config/Config.h"
+#include "../display/DisplayManager.h"
+#include "../utils/FancyLog.h"
+#include "OTAManager.h"
+
+class NetworkManager {
+  public:
+    NetworkManager(DisplayManager& display, FancyLog& fancyLog);
+    void begin();
+    bool connectWiFi();
+    bool sendHttpPostRequest(String jsonPayload, String apiRoute);
+    void checkForUpdates();
+    void pollOTA();
+    bool isConnected() { return WiFi.status() == WL_CONNECTED; }
+
+  private:
+    DisplayManager& display;
+    FancyLog& fancyLog;
+    WiFiClient wifiClient;
+    bool updateAvailable;
+    String latestFirmwareVersion;
+    bool handleUpdateResponse(String& response);
+    bool downloadAndApplyUpdate(String& downloadUrl, int firmwareSize);
+};
+
+#endif // NETWORK_MANAGER_H 
---
+++ H2Climate_v0.8.0_uno_r4/src/sensors/SensorManager.h
@@ -0,0 +1,20 @@
+#ifndef SENSOR_MANAGER_H
+#define SENSOR_MANAGER_H
+
+#include "../config/Config.h"
+#include "../utils/FancyLog.h"
+#include "DHT.h"
+
+class SensorManager {
+  public:
+    SensorManager(FancyLog& fancyLog);
+    void begin();
+    float readTemperature();
+    float readHumidity();
+    DHT dht; // Public DHT instance for legacy reasons
+
+  private:
+    FancyLog& fancyLog;
+};
+
+#endif // SENSOR_MANAGER_H 
